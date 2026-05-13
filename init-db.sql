eea6b76b6a01eec51492b5a76270324303e94566
aryanyadav2003 <yadavaryadav13@gmail.com>
2026-05-13 10:19:23 +0530

    Initialize project structure with base files

---
+++ init-db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE flowboard_auth;
+CREATE DATABASE flowboard_workspace;
+CREATE DATABASE flowboard_board;
+CREATE DATABASE flowboard_list;
+CREATE DATABASE flowboard_card;
+CREATE DATABASE flowboard_comment;
+CREATE DATABASE flowboard_label;
+CREATE DATABASE flowboard_checklist;
